3b9ec1d33b96295e207c173e4cea916125c5f947
Jacobus Geluk <jacobus.geluk@agnos.ai>
2022-02-21 12:24:28 +0000

    #1 minor changes

--- docs/main.py
+++ docs/main.py
@@ -3,9 +3,10 @@ def define_env(env):
 
     @env.macro
     def test123(page):
-        list = []
-        while page.next_page:
-            list.append(page.next_page)
-            page = page.next_page
-        return list
+        page_list = []
+        this_page = page.next_page
+        while this_page:
+            page_list.append(this_page)
+            this_page = this_page.next_page
+        return page_list
     
